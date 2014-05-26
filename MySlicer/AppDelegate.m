5c6576cc55f6565f6ca13db4a4ad5d050001f115
idolenvy <detherorc@gmail.com>
2014-05-26 13:02:39 +0700

    1st Commit
    1st Commit

--- MySlicer/AppDelegate.m
+++ MySlicer/AppDelegate.m
@@ -14,6 +14,9 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    
+    NSLog(@"TEST");
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
